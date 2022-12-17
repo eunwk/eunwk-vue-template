945571875b92ea89d03fe8bf069f233034b13d89
eunwk <eunwk@naver.com>
2022-12-17 19:37:08 +0900

    폴더경로수정

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify',
   ],
-  outputDir: '/eunwk-vue-template/vue2-vuetify2/dist',
-  publicPath: '/eunwk-vue-template/vue2-vuetify2/'
+  outputDir: './dist',
+  publicPath: '/eunwk-vue-template'
 });
