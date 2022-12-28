7ba5bbbfc8d6242d78e2fa6d4fa08682acaee514
eunwk <eunwk@naver.com>
2022-12-28 21:29:39 +0900

    Updates

--- js/about.6e2f2d26.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkvue2_vuetify2"]=self["webpackChunkvue2_vuetify2"]||[]).push([[443],{5652:function(u,t,e){e.r(t),e.d(t,{default:function(){return r}});var n=function(){var u=this;u._self._c;return u._m(0)},s=[function(){var u=this,t=u._self._c;return t("div",{staticClass:"about"},[t("h1",[u._v("This is an about page")])])}],i=e(3736),a={},l=(0,i.Z)(a,n,s,!1,null,null,null),r=l.exports}}]);
-//# sourceMappingURL=about.6e2f2d26.js.map
